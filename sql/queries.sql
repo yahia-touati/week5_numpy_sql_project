28bb816e7e3d301301ef59ba828cab18facd2b87
yahia <yahiatouati75@gmail.com>
2026-09-19 12:00:15 +0100

    fix: correct join condition in Q3 (oi.product_id) ;

--- sql/queries.sql
+++ sql/queries.sql
@@ -9,7 +9,7 @@ join products p on p.product_id = ot.product_id
 GROUP BY p.category
 ORDER BY wills desc;
 
---
+-- Display the number od customer orders and their names
 select
     c.name,
     count(o.order_id) as order_number
@@ -17,3 +17,23 @@ from customers c
 join orders o on c.customer_id = o.customer_id
 group by c.name
 order by order_number desc;
+
+--
+with total_revenue as(
+    SELECT
+        o.customer_id,
+        sum(p.price * oi.quantity) as total
+    from products p
+    join order_items oi on p.product_id = oi.product_id
+    join orders o on o.order_id = oi.order_id
+    GROUP BY o.customer_id 
+)
+SELECT 
+    c.name,
+    c.city,
+    tr.total
+from customers c
+join total_revenue tr on tr.customer_id = c.customer_id
+ORDER BY tr.total desc 
+limit 5;
+
