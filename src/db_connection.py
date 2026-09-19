cf0a796671cdb4da200ab44ba4c1603e6a755157
yahia <yahiatouati75@gmail.com>
2026-09-19 14:42:36 +0100

    feat: add database connect module

---
+++ src/db_connection.py
@@ -0,0 +1,19 @@
+import os 
+import psycopg2
+from dotenv import load_dotenv
+
+def get_connection():
+    load_dotenv()
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
+    print("Connected successfully!")
+    return conn
+
+if __name__ == "__main__":
+    conn = get_connection()
+    conn.close()
